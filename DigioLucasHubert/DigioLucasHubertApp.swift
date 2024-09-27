1f96bc10696f9d3d59e6c4f78b5d0977491137be
Lucas Jorge Hubert <lucasjhubert@gmail.com>
2024-09-27 17:12:17 -0300

    Initial Commit

---
+++ DigioLucasHubert/DigioLucasHubertApp.swift
@@ -0,0 +1,17 @@
+//
+//  DigioLucasHubertApp.swift
+//  DigioLucasHubert
+//
+//  Created by Lucas Hubert on 27/09/24.
+//
+
+import SwiftUI
+
+@main
+struct DigioLucasHubertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
